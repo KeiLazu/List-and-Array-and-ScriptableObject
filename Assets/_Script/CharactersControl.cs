eb80b6f5cf83b907db2a08d1e9dc615c4f0f0f98
KeiLazu <kennixlazuardi@gmail.com>
2017-04-17 20:04:51 +0700

    First Commit

---
+++ Assets/_Script/CharactersControl.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharactersControl : MonoBehaviour {
+
+    [SerializeField]
+    public List<ListCharacters> characters;
+    protected ListCharacters CharactersClass;
+
+    protected int counting;
+    protected int randomPower;
+
+    [SerializeField]
+    Text[] lblNameChara;
+    [SerializeField]
+    Text[] lblPowerChara;
+
+	// Use this for initialization
+	void Start () {
+
+        characters = new List<ListCharacters>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            
+            Randomizing();
+            characters.Add(new ListCharacters("Natsume", randomPower));
+            Debug.Log("Added: Natsume - " + randomPower);
+            status();
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+
+            characters.RemoveAt(counting-1);
+            status();
+        }
+		
+	}
+
+    void Randomizing()
+    {
+        randomPower = UnityEngine.Random.Range(0, 100);
+    }
+
+    void status()
+    {
+
+        counting = characters.Count;
+        Debug.Log("Remaining: " + counting);
+
+        /**
+         * i'll fix this to be an automaton, i mean auto insert into those text, 
+         * next up: sprites, page and scriptableobjects
+         */
+
+        try
+        {
+            if (counting > 0)
+            {
+                lblNameChara[0].text = "Name: " + characters[0].get_nameChara;
+                lblPowerChara[0].text = "Power: " + characters[0].get_powerChara;
+            } else { Debug.Log("Empty"); lblNameChara[0].text = ""; lblPowerChara[0].text = ""; }
+        } catch (Exception e)
+        {
+            Debug.Log("No Array Here, Boss \n" + e);
+        }
+    }
+}
---
+++ Assets/_Script/ListCharacters.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListCharacters
+{
+
+    protected string _nameChara;
+    protected int _powerChara;
+
+    public string get_nameChara
+    {
+        get { return _nameChara; }
+        set { _nameChara = value; }
+    }
+
+    public int get_powerChara
+    {
+        get { return _powerChara; }
+        set { _powerChara = value; }
+    }
+
+    public ListCharacters()
+    {
+        //i kindly call this one "zeroing"
+    }
+
+    public ListCharacters(string nameChara, int powerChara)
+    {
+
+        _nameChara = nameChara;
+        _powerChara = powerChara;
+    }
+
+}
